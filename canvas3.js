bc28d6d9bfd43e389a0dc509a0a5f09ca9fb8ee5
Emin1996 <30591992+Emin1996@users.noreply.github.com>
2017-08-06 18:16:35 +0200

    Add files via upload

---
+++ canvas3.js
@@ -0,0 +1,42 @@
+var c2=document.getElementById("mycanvas3");
+
+var cty=c2.getContext("2d");
+/*
+cty.beginPath();
+cty.arc(790,238,15,0*Math.PI,2*Math.PI);
+cty.strokeStyle='#3AAD55';
+cty.fillStyle='#3AAD55';
+cty.stroke();
+*/
+cty.beginPath();
+cty.rect(80, 80, 820, 300);
+cty.strokeStyle="black";
+cty.stroke();
+
+cty.beginPath();
+cty.arc(783, 220, 50, 1.1*Math.PI, 0.85*Math.PI);
+cty.strokeStyle="black";
+cty.stroke();
+
+cty. beginPath();
+cty.moveTo(737, 204);
+cty.lineTo(700, 204);
+cty.stroke();
+
+cty.beginPath();
+cty.moveTo(738,243);
+cty.lineTo(700,243);
+cty.stroke();
+
+cty.beginPath();
+cty.arc(785,220,15,0*Math.PI,2*Math.PI);
+cty.strokeStyle='#3AAD55';
+cty.fillStyle='#3AAD55';
+cty.stroke();
+
+cty.beginPath();
+cty.beginPath();
+cty.arc(113,220,10,0*Math.PI,2*Math.PI);
+//ctx.strokeStyle='black';
+cty.fillStyle = 'white';
+cty.fill();
